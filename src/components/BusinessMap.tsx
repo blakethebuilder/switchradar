339bb543224d92dbe670c77e92df093965111af8
Blake Foster <blake@smartintegrate.co.za>
2026-02-04 11:19:11 +0200

    test marker

--- src/components/BusinessMap.tsx
+++ src/components/BusinessMap.tsx
@@ -272,6 +272,22 @@ export const BusinessMap: React.FC<BusinessMapProps> = ({
           selectedBusinessIds={selectedBusinessIds}
           onBusinessSelect={onBusinessSelect}
         />
+        
+        {/* Debug: Add a test marker to verify map is working */}
+        {businesses.length > 0 && (
+          <Marker
+            position={[-26.8521, 26.6667]}
+            icon={L.icon({
+              iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+              iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+              shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowSize: [41, 41]
+            })}
+          />
+        )}
       </MapContainer>
 
       {/* Map controls */}
